f0d176da15df8c65ea92c03f46a81617919e83ab
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2019-05-06 00:31:37 +0800

    支持改变地盘角度

--- packages/client/src/game_objects/tank.ts
+++ packages/client/src/game_objects/tank.ts
@@ -1,34 +1,45 @@
 import { GraphicsContext } from '../engine/graphics_context'
 import { GameObject } from '../engine/game_object'
+import { Rotation2d } from '../engine/rotation'
 
 export class Tank extends GameObject {
   graphicsContext: GraphicsContext
+  wheelRotation: Rotation2d
 
   constructor(graphicsContext: GraphicsContext) {
     super()
     this.graphicsContext = graphicsContext
+    this.wheelRotation = new Rotation2d(0)
   }
 
   draw() {
     const ctx = this.graphicsContext.ctx
-    ctx.save()
-    ctx.fillStyle = 'red'
     const { x, y } = this.position
-    const fillColor = 'red'
-    ctx.fillStyle = fillColor
+    ctx.save()
+
+    ctx.translate(x + 10, y + 11)
+    ctx.rotate((this.wheelRotation.rotation * Math.PI) / 180)
+    ctx.translate(-(x + 10), -(y + 11))
+
+    ctx.fillStyle = '#300ccf'
     ctx.fillRect(x, y - 1, 5, 22)
     ctx.fillRect(x + 15, y - 1, 5, 22)
     ctx.fillRect(x + 6, y, 8, 20)
-    ctx.fillStyle = 'blue'
+    ctx.rotate(-(45 * Math.PI) / 180)
+    ctx.restore()
+    ctx.save()
+
+    ctx.strokeStyle = '#085a91'
+    ctx.lineWidth = 3
+    ctx.moveTo(x + 10, y + 5)
+    ctx.lineTo(x + 10, y - 5)
+    ctx.stroke()
+
+    ctx.fillStyle = '#0c81cf'
     ctx.beginPath()
     ctx.arc(x + 10, y + 10, 3, 0, Math.PI * 2, true)
     ctx.closePath()
     ctx.fill()
-    ctx.strokeStyle = 'orange'
-    ctx.lineWidth = 2
-    ctx.moveTo(x + 10, y + 10)
-    ctx.lineTo(x + 10, y - 10)
-    ctx.stroke()
     ctx.restore()
   }
 }
